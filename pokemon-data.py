1b6d9f3179bf2a514654d6e9d38fffde4cadd855
JD <67335440+janet-dev@users.noreply.github.com>
2021-03-31 19:00:37 +0100

    Update pokemon-data.py

--- pokemon-data.py
+++ pokemon-data.py
@@ -4,7 +4,7 @@ Use a list to store 6 Pokemon IDs.
 Then in a 'for' loop call the API to retrieve the data for each Pokemon.
 Save their names and moves into a file called 'pokemon.txt'
 """
-# import csv
+
 import requests
 
 pokemon_ids = []  # initialise the Pokemon ID list
